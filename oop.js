fc03027de7f41a5bd9d96023b75839b14110d7fa
Juma Qasimi <Jumaqasimi@gmail.com>
2024-10-26 16:12:42 +0200

    create person class in oop file

--- oop.js
+++ oop.js
@@ -1,2 +1,18 @@
 // OOP in javaScript
 
+
+class Person{
+	// Parent class
+	constructor(name, dateOfBirth){
+		this.name = name
+		this.dateOfBirth = dateOfBirth
+	}
+
+	PrintInfo(){
+		const currentDate = new Date();
+		const age = currentDate.getFullYear() - this.dateOfBirth;
+
+		console.log('Mr./Mis.' + this.name +' is '+ age +'Years old!')
+	}
+
+}
