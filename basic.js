0c76f5d6872d13ab787760bf2d56f05bbaee1cfa
Juma Qasimi <Jumaqasimi@gmail.com>
2024-11-06 19:09:25 +0100

    create html file

--- basic.js
+++ basic.js
@@ -1,6 +1,12 @@
+// html document
+
+let testEle = document.getElementById('test');
+testEle.style.backgroundColor ='lightgreen'
+
 const num1 = [1,2,34,5];
 const num2 = [12,24,34,55];
 
 const sliec = [...num1,...num2];
-console.log(sliec); // nicht copy
+
+testEle.innerHTML = sliec;
 
