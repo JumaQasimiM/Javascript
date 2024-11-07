ce4513992672beb178f1f944df4276c3fc9cdca7
Juma Qasimi <Jumaqasimi@gmail.com>
2024-11-07 20:31:47 +0100

    create funtion file

--- functions.js
+++ functions.js
@@ -1,4 +1,25 @@
 const show_result = document.querySelector('#test');
 
+// Function Decleration 
+function hayHello(){
+	show_result.innerHTML = 'hello world!'
+}
+// hayHello()
 
+// function Experation
 
+let odd_even = function(number){
+	let result ='';
+	if (number == 0){
+		result = 'Zero';
+	}
+	else if(number%2==0){
+		result = 'Even;'
+	}
+	else{
+		result = 'Odd';
+	}
+	return show_result.innerHTML = result;
+}
+
+odd_even(089)
