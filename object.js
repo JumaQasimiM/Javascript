4e3d4fbda703c0e9b9b70fba382a171d64dffb0d
Juma Qasimi <Jumaqasimi@gmail.com>
2024-11-08 23:35:59 +0100

    create object inside object file

---
+++ object.js
@@ -0,0 +1,23 @@
+const show_result = document.querySelector('#test');
+
+const Person ={
+	// getter --> access property
+	/* add get or set key word befor function name */ 
+	// setter -- change or set value
+	name:'Juma',
+	lastName:'qasimi',
+	get fullName(){
+		return `${Person.name} ${Person.lastName}`;
+	},
+	set fullName(name){
+		const parts = name.split(' ');
+		 this.name = parts[0];
+		this.lastName = parts[1];
+
+	}
+
+};
+
+Person.fullName = 'Ahmad Ahmadi';
+// show_result.innerHTML  = Person.fullName;
+show_result.innerHTML  = Person;
