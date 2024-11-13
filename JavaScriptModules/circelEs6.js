20daa94991b74bbefb0fc7b53535de2818eedc7e
Juma Qasimi <Jumaqasimi@gmail.com>
2024-11-14 00:20:59 +0100

    modified es6 file

--- JavaScriptModules/circelEs6.js
+++ JavaScriptModules/circelEs6.js
@@ -1,4 +1,3 @@
-import { _radius } from "./circle";
 
 const _radius = new WeakMap();
 export default class Circel{
@@ -9,13 +8,3 @@ export default class Circel{
         console.log('Circel radius :'+ _radius.get(this));
     }
 }
-
-class Circel {
-    constructor(radius) {
-        _radius.set(this, radius);
-    }
-    drow() {
-        console.log('Circel radius :' + _radius.get(this));
-    }
-}
-exports.Circel = Circel;
