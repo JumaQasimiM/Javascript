4530fe0fe8483d879363212f7db90b39a51e511b
Juma Qasimi <Jumaqasimi@gmail.com>
2024-11-13 23:36:00 +0100

    inhrentance

---
+++ classInhretanc.js
@@ -0,0 +1,24 @@
+class Animal{
+    constructor(name){
+        this.name = name;
+    }
+    move(){
+        console.log('move form Animal');
+    }
+}
+class Cat extends Animal{
+    constructor(name, age){
+        super(name); // form Parent
+        this.age = age;
+    }
+    move(){
+        // method overriding
+        super.move(); // parent method
+        console.log('cat move!');
+    }
+    print(){
+        console.log(this.age,this.name);
+    }
+
+}
+const tom = new Cat('Tom',3);
