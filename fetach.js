f2e21c5772f867dfa8a6540159f180f07ef5977f
Juma Qasimi <Jumaqasimi@gmail.com>
2024-12-25 12:26:04 +0100

    CROD fetach api

---
+++ fetach.js
@@ -0,0 +1,69 @@
+
+const new_user ={
+    id:1000,
+    first_name:'juma',
+    last_name:'Qasimi'
+}
+
+fetch('https://reqres.in/api/users',{
+    method:'POST',
+    headers:{
+        'Content-type':'Application/json',
+    },
+    body:JSON.stringify(new_user),
+
+})
+.then(res=> res.json())
+.then(data=>console.log(data));
+
+
+
+
+// GET data from api
+fetch('https://reqres.in/api/users')
+ .then(response=>
+    {
+        if(!response.ok){
+            throw new Error('Problem!');
+            return;
+        }
+        else{
+        return response.json()
+        }
+    })
+ .then(data=>console.log(data))
+ .catch(err=>console.log(err));
+ 
+// UPDATE DATA
+
+const new_user_update ={
+    id:1000,
+    first_name:'Mohammad',
+    last_name:'Qasimi'
+}
+
+fetch('https://reqres.in/api/users/1000',{
+    method:'PUT',
+    headers:{
+        'Content-type':'Application/json',
+    },
+    body:JSON.stringify(new_user_update),
+
+})
+.then(res=> res.json())
+.then(data=>console.log(data))
+.catch(err=>console.log(err));
+
+// DELETE
+
+
+fetch('https://reqres.in/api/users/1000',{
+    method:'DELETE'
+})
+.then(res=> {
+   console.log('deleted!');
+})
+
+.then(data=>console.log(data))
+
+.catch(err=>console.log(err));
