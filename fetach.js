792756d4f96fcbbe3884129d0ceb1cf7c408c34b
Mohammad Qasimi <qasimimohammad819@gmail.com>
2025-03-14 23:28:16 +0100

    Update fetach.js
    create try and catch for product  fetch 
    api

--- fetach.js
+++ fetach.js
@@ -2,7 +2,8 @@
 
 fetch('https://reqres.in/api/product')
     .then(res =>res.json())
-    .then(data=>console.log(data));
+    .then(data=>console.log(data))
+    .catch(err=>console.log(err));
 
 const new_user ={
     id:100,
