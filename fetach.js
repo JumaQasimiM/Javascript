087929fc37c1c511cebd1bea74202102747b33d2
Mohammad Qasimi <qasimimohammad819@gmail.com>
2025-03-16 00:48:11 +0100

    Update fetach.js

--- fetach.js
+++ fetach.js
@@ -4,7 +4,7 @@ fetch('https://reqres.in/api/product')
     .then(res =>res.json())
     .then(data=>console.log(data))
     .catch(err=>console.log(err));
-
+// create 
 const new_user ={
     id:100,
     first_name:'juma',
