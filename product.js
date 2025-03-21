036db639c81cfb22a4c5e24697ca7974f9116ffc
Mohammad Qasimi <qasimimohammad819@gmail.com>
2025-03-21 19:39:22 +0100

    Update product.js
    read and show product form api

--- product.js
+++ product.js
@@ -1,3 +1,23 @@
 // read product from api
 
-
+const url = 'https://fakestoreapi.com/products';
+fetch(url)
+  .then(resp => response.json())
+  .then(data => displayProducts(data))
+  .catch(err=>
+    console.log('Error :',err))
+function displayProducts(products){
+  const productLit = document.getElemetById('product');
+  products.forEach(product => {
+    const item = documentElement('div');
+    item.classList.add('product');
+    item.innerHTML = `
+    <h3>
+    ${product.title}
+    <h3/>
+    <img src = "${product.image}">
+    <p>Price: ${product.price}</p>
+    ´;
+    productLit.appendChild(item);
+  });
+}
