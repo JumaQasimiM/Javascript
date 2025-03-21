81ea5ddc99fc8a861ec30ea737f3b719a8f8a086
Mohammad Qasimi <qasimimohammad819@gmail.com>
2025-03-21 20:11:55 +0100

    Update product.js

--- product.js
+++ product.js
@@ -9,17 +9,5 @@ fetch('https://fakestoreapi.com/products')
 
 // read api function
 function displayProducts(products){
-  const productLit = document.getElementById('product_list');
-  products.forEach(product => {
-    const item = document.createElement('div');
-    item.classList.add('product');
-    item.innerHTML = `
-    
-    <h3> ${product.title} <h3/>
-    <img src = "${product.image}">
-    <p>Price: ${product.price}</p>
-    
-    ´;
-    productLit.appendChild(item);
-  });
+ 
 }
