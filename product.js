94acf9d1e661c38352a4254b7c2848d46e8a468d
Mohammad Qasimi <qasimimohammad819@gmail.com>
2025-08-17 23:49:34 +0200

    Add files via upload
    created weather app

--- product.js
+++ product.js
@@ -4,24 +4,28 @@
 fetch('https://fakestoreapi.com/products?limit=4')
   .then(resp => resp.json())
   .then(data => displayProducts(data))
-  .catch(err=>
-    console.log('Error :',err));
+  .catch(err =>
+    console.log('Error :', err));
 
 // // read api function
-function displayProducts(products){
+function displayProducts(products) {
   const productLit = document.getElementById('product_list');
-  
-  
+
+
   products.forEach(product => {
-    
+
     const item = document.createElement('div');
-    
+
     item.classList.add('product');
 
     item.innerHTML =
-   ` <div id='product'><h3 class='text-blue-600 text-center p-3'> ${product.title} <h3/> 
-    <img src = "${product.image}" class='rounded' width='200' height='200'> <p>Price: ${product.price}</p>
-    <button>Add</button>
+      ` 
+ 
+   <div id='product' class='product-card'>
+      <img src = "${product.image}" class='img'>
+      <h3 class='title'> ${product.title} <h3/> 
+      <p class='price'> ${product.price} $</p>
+      <button class='btn'>Add to Cart</button>
     </div>
     `;
 
@@ -29,3 +33,15 @@ function displayProducts(products){
     productLit.appendChild(item);
   });
 };
+// for menu bar
+const menubar = document.querySelector('.menu-bar');
+menubar.addEventListener('click', () => {
+  console.info('menu bar clicked');
+  const ul = document.querySelector('ul');
+  ul.classList.toggle('open');
+  if (ul.classList.contains('open')) {
+    menubar.innerHTML = '👆🏾';
+  } else {
+    menubar.innerHTML = '👇🏾';
+  }
+})
---
+++ weather.js
@@ -0,0 +1,67 @@
+// read product from api
+
+const apiKey = '8df82f3bcf37ccfe82166b9ed9a939b7';
+// const city = 'heilbronn';
+
+const searchBtn = document.getElementById('btn');
+
+searchBtn.addEventListener('click', () => {
+  const city = document.getElementById('city').value.trim();
+  if (!city) return alert('Please enter city name');
+  const api_url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+
+
+  fetch(api_url)
+    .then(
+      resp => resp.json())
+
+    .then(data =>
+      // console.log(data)
+      // document.getElementById('err').innerText = '',
+
+      document.getElementById('weather').innerHTML =
+      ` 
+     <div id='weather' class='weather-card'>
+      <p class='desc'> ${(data.weather[0].description) === "clear sky" ? "🌞" : "🌧️"} </p >
+      <p class='temp'> ${data.main.temp} °C <p/>
+
+    <div class="info">
+       <div class='weather-info-city'>
+       <h3 class='city'> ${data.name} <h3/> 
+        <h3 class='country'> ${data.sys.country} <h3/> </div>
+
+        <div class='weather-info-temp'>
+        <p class='temp'> ${data.weather[0].main}  <p/>
+        <h3 class='time'> ${new Date((data.dt + data.timezone) * 1000).toUTCString().replace('GMT', 'UTC')} <h3/>
+        </div>
+    </div>
+
+
+
+      </div >
+
+
+    `
+
+    )
+    .catch(err =>
+
+      console.log('Error :', err));
+
+  document.getElementById('weather').innerHTML = '';
+  document.getElementById('err').innerText = 'city not found';
+})
+
+// for menu bar
+const menubar = document.querySelector('.menu-bar');
+menubar.addEventListener('click', () => {
+  console.info('menu bar clicked');
+  const ul = document.querySelector('ul');
+  ul.classList.toggle('open');
+  if (ul.classList.contains('open')) {
+    menubar.innerHTML = '👆🏾';
+  } else {
+    menubar.innerHTML = '👇🏾';
+  }
+})
