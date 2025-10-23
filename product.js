22021d1bbc6ee4e89a6ddd102e79469b5e3e07e0
M Juma <qasimimohammad819@gmail.com>
2025-10-23 15:20:17 +0200

    modified index.html

--- product.js
+++ product.js
@@ -1,47 +1,46 @@
 // read product from api
 
-
-fetch('https://fakestoreapi.com/products?limit=12')
-  .then(resp => resp.json())
-  .then(data => displayProducts(data))
-  .catch(err =>
-    console.log('Error :', err));
+fetch("https://fakestoreapi.com/products?limit=12")
+  .then((resp) => resp.json())
+  .then((data) => displayProducts(data))
+  .catch((err) => console.log("Error :", err));
 
 // // read api function
 function displayProducts(products) {
-  const productLit = document.getElementById('product_list');
+  const productLit = document.getElementById("product_list");
 
+  products.forEach((product) => {
+    const item = document.createElement("div");
 
-  products.forEach(product => {
+    item.classList.add("product");
 
-    const item = document.createElement('div');
-
-    item.classList.add('product');
-
-    item.innerHTML =
-      ` 
- 
-   <div id='product' class='product-card'>
-      <img src = "${product.image}" class='img'>
-      <h3 class='title'> ${product.title} <h3/> 
-      <p class='price'> ${product.price} $</p>
-      <button class='btn'>Add to Cart</button>
-    </div>
+    item.innerHTML = ` 
+   
+    <div id='product' class='flex flex-col justify-center items-center shadow-lg'>
+        <a href='pages/products/${product.id}'>
+        <img src = "${product.image}"class='w-full h-56'>
+    </a>
+        <div class='w-full h-30 text-center'>
+          <h3 class='title'> ${product.title.slice(0, 40)} <h3/> 
+          <p class='price'> ${product.price} $</p>
+          <button class='bg-gray-200 btn'>Add to Cart</button>
+        </div>
+      </div>
+  
     `;
-
 
     productLit.appendChild(item);
   });
-};
+}
 // for menu bar
-const menubar = document.querySelector('.menu-bar');
-menubar.addEventListener('click', () => {
-  console.info('menu bar clicked');
-  const ul = document.querySelector('ul');
-  ul.classList.toggle('open');
-  if (ul.classList.contains('open')) {
-    menubar.innerHTML = '👆🏾';
+const menubar = document.querySelector(".menu-bar");
+menubar.addEventListener("click", () => {
+  console.info("menu bar clicked");
+  const ul = document.querySelector("ul");
+  ul.classList.toggle("open");
+  if (ul.classList.contains("open")) {
+    menubar.innerHTML = "x";
   } else {
-    menubar.innerHTML = '👇🏾';
+    menubar.innerHTML = "⫩";
   }
-})
+});
